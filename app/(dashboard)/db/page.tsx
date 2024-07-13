687e9de37e915cd605849b816fd5bd610a8edd2c
Bret Petersen <bretpetersen82@gmail.com>
2024-07-12 21:12:39 -0500

    Commented out incomplete code in order to publish and test completed code

--- app/(dashboard)/db/page.tsx
+++ app/(dashboard)/db/page.tsx
@@ -1,34 +1,35 @@
-// import { createTodo } from "@/utils/api"
-// import { useState } from "react"
-import { prisma } from "@/utils/db"
+// // import { createTodo } from "@/utils/api"
+// // import { useState } from "react"
+// import { prisma } from "@/utils/db"
+// // import History from "@/components/History"
+
+// import AddTodo from "@/components/AddTodo"
 // import History from "@/components/History"
+// import HistoryTitle from "@/components/HistoryTitle"
 
-import AddTodo from "@/components/AddTodo"
-import History from "@/components/History"
-import HistoryTitle from "@/components/HistoryTitle"
-
-const getDataUser01 = async () => {
-  const content = await prisma.affgroup.findMany({
-    // where: {
-    //   NOT: {
-    //     title: {
-    //       contains: "Hardcoded db entry" /* Optional filter */,
-    //     },
-    //   },
-    // },
-    orderBy: {
-      id: "asc",
-    },
-  })
-  return content
-}
+// const getDataUser01 = async () => {
+//   const content = await prisma.affgroup.findMany({
+//     // where: {
+//     //   NOT: {
+//     //     title: {
+//     //       contains: "Hardcoded db entry" /* Optional filter */,
+//     //     },
+//     //   },
+//     // },
+//     orderBy: {
+//       id: "asc",
+//     },
+//   })
+//   return content
+// }
 
 const DbPage = async () => {
-  const content = await getDataUser01()
+  // const content = await getDataUser01()
   return (
     <div className="">
-      <AddTodo />
-      <History data={content} />
+      Database
+      {/* <AddTodo />
+      <History data={content} /> */}
     </div>
   )
 }
